b84d5e7d45c728ddb11f3ba80b28f7b76c3ea932
j6k1 <jinpu@s254.xrea.com>
2024-02-04 13:21:16 +0900

    Performance Tuning Performance tests showed that it is much faster to loop over arrays with subscript references, so the complex implementation was abandoned and the loop was changed to loop over arrays with subscripts.

--- src/reshape.rs
+++ src/reshape.rs
@@ -5,88 +5,65 @@ use crate::{Assert, assert_convolution, IsTrue};
 
 use crate::collection::{ImageView};
 
+#[inline]
 pub fn im2col<'a,T,const H:usize,const W:usize,const FH:usize,const FW:usize,const PAD:usize,const S:usize>(image:&ImageView<'a,T,H,W>)
     -> Arr2<T,{ ((H + PAD * 2 - FH) / S + 1) * ((W + PAD * 2 - FW) / S + 1) }, { FH * FW }>
     where T: Default + Clone + Copy + Send + Sync + 'static,
           Assert<{ assert_convolution::<H,W,FH,FW,PAD,S>() }>: IsTrue {
-    let image = image.as_raw_slice();
-
-    let ys = (H + PAD * 2 - FH) / S + 1;
     let xs = (W + PAD * 2 - FW) / S + 1;
 
-    let yskiped = (PAD as isize - FH as isize).max(0) as usize / S + (PAD / FH).min(1);
-    let xskiped = (PAD as isize - FW as isize).max(0) as usize / S + (PAD / FW).min(1);
-    let distance = (FW + S - 1) / S * S;
-    let xc = distance / S;
-    let xc = xc.min(W + PAD * 2 - (xc - 1) * S);
+    let mut r = Arr2::<T,{ ((H + PAD * 2 - FH) / S + 1) * ((W + PAD * 2 - FW) / S + 1) }, { FH * FW }>::new();
 
-    let mut r = vec![T::default(); ys * FH * xs * FW];
+    for y in 0..((H + PAD * 2 - FH) / S + 1) {
+        for x in 0..((W + PAD * 2 - FW) / S + 1) {
+            if FW * FH >= 8 {
+                let chunk_size = FW * FH / 8;
 
-    for y in yskiped..(ys - yskiped) {
-        let p = (PAD as isize - (y * S) as isize).max(0) as usize;
+                for c in 0..8 {
+                    for index in 0..chunk_size {
+                        let index = c * chunk_size + index;
 
-        for ly in (0..FH).skip(p).take((FH as isize + ((H + PAD) as isize - (FH + y * S) as isize).min(0)) as usize - p) {
-            for x in 0..xc {
-                let x = x + xskiped;
+                        let i = index / FW;
+                        let j = index - i * FW;
+                        let sy = y * S + i;
+                        let sx = x * S + j;
 
-                let rp = PAD.min(x * S);
-                let sx = (PAD as isize - (x * S) as isize).max(0).min(FW as isize) as usize * (x * S <= PAD) as usize;
-                let cw = (FW - sx).min(W - (x * S - rp));
-                let dst_start_offset = (y * FH * xs + (ly + x * FH)) * FW + sx;
-                let src_start_offset = ((y * S + ly) - PAD) * W + x * S - rp;
-
-                for (d,s) in (&mut r[
-                    dst_start_offset..(dst_start_offset + FW - sx)
-                ]).iter_mut().zip((&image[src_start_offset..(src_start_offset + cw)]).iter()) {
-                    *d = *s;
-                }
-
-                let rp = PAD.min(x  * S + distance);
-
-                if distance / S + x >= xs || x * S + distance >= W + PAD {
-                    continue;
-                }
-
-                let dist = distance / S;
-
-                let dst_start_offset = (y * FH * xs + ly + dist * FH) * FW + x * FW * FH;
-                let src_start_offset = ((y * S + ly) - PAD) * W + x * S + distance - rp;
-
-                let dst_end_offset = (y * FH * xs) * FW + xs * FH * FW;
-                let src_end_offset = src_start_offset + W + rp - (x * S + distance);
-
-                let chunk_size = (dist + x) / 8;
-
-                if chunk_size > 0 {
-                    let chunks = (&mut r[
-                        dst_start_offset..dst_end_offset
-                    ]).chunks_mut(FW).step_by(FH * dist).zip((&image[
-                        src_start_offset..src_end_offset
-                    ]).chunks(distance));
-
-                    let chunks_iter = chunks.chunks(chunk_size);
-
-                    for (_,chunk) in (0..8).zip(chunks_iter.into_iter()) {
-                        for (d,s) in chunk.into_iter() {
-                            for (d,s) in d.iter_mut().zip(s.iter()) {
-                                *d = *s;
-                            }
+                        if sy >= PAD && sy < H + PAD && sx >= PAD && sx < W + PAD {
+                            r[(y * xs + x, i * FW + j)] = image[(
+                                sy - PAD, sx - PAD
+                            )];
                         }
                     }
                 }
 
-                for (d,s) in (&mut r[
-                    dst_start_offset..dst_end_offset
-                ]).chunks_mut(FW).step_by(FH * dist).zip((&image[
-                    src_start_offset..src_end_offset
-                ]).chunks(distance)).skip(chunk_size * 8) {
-                    for (d,s) in d.iter_mut().zip(s.iter()) {
-                        *d = *s;
+                for index in (chunk_size * 8)..(FH * FW) {
+                    let i = index / FW;
+                    let j = index - i * FW;
+                    let sy = y * S + i;
+                    let sx = x * S + j;
+
+                    if sy >= PAD && sy < H + PAD && sx >= PAD && sx < W + PAD {
+                        r[(y * xs + x, i * FW + j)] = image[(
+                            sy - PAD, sx - PAD
+                        )];
+                    }
+                }
+            } else {
+                for i in 0..FH {
+                    for j in 0..FW {
+                        let sy = y * S + i;
+                        let sx = x * S + j;
+
+                        if sy >= PAD && sy < H + PAD && sx >= PAD && sx < W + PAD {
+                            r[(y * xs + x, i * FW + j)] = image[(
+                                sy - PAD, sx - PAD
+                            )];
+                        }
                     }
                 }
             }
         }
     }
 
-    return r.try_into().unwrap();
+    r
 }
